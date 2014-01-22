102a3105cf0515e9a891973eaefdba3074139d5d
HafizAhmed <hafizeltayeb@gmail.com>
2014-01-22 14:29:12 -0800

    add version1

---
+++ MyActivitiesPlanner_version1/MyActivitiesPlanner/MyActivitiesPlanner/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace MyActivitiesPlanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddActv = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.ActTB = new System.Windows.Forms.TextBox();
+            this.STimeTB = new System.Windows.Forms.TextBox();
+            this.ETimeTB = new System.Windows.Forms.TextBox();
+            this.Actv = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.Show_Button = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddActv
+            // 
+            this.AddActv.AutoSize = true;
+            this.AddActv.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddActv.Location = new System.Drawing.Point(3, 0);
+            this.AddActv.Name = "AddActv";
+            this.AddActv.Size = new System.Drawing.Size(116, 18);
+            this.AddActv.TabIndex = 0;
+            this.AddActv.Text = "Add an activity";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(9, 33);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // ActTB
+            // 
+            this.ActTB.Location = new System.Drawing.Point(76, 65);
+            this.ActTB.Name = "ActTB";
+            this.ActTB.Size = new System.Drawing.Size(100, 20);
+            this.ActTB.TabIndex = 2;
+            // 
+            // STimeTB
+            // 
+            this.STimeTB.Location = new System.Drawing.Point(76, 116);
+            this.STimeTB.Name = "STimeTB";
+            this.STimeTB.Size = new System.Drawing.Size(100, 20);
+            this.STimeTB.TabIndex = 3;
+            // 
+            // ETimeTB
+            // 
+            this.ETimeTB.Location = new System.Drawing.Point(321, 116);
+            this.ETimeTB.Name = "ETimeTB";
+            this.ETimeTB.Size = new System.Drawing.Size(100, 20);
+            this.ETimeTB.TabIndex = 4;
+            // 
+            // Actv
+            // 
+            this.Actv.AutoSize = true;
+            this.Actv.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Actv.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Actv.Location = new System.Drawing.Point(7, 65);
+            this.Actv.Name = "Actv";
+            this.Actv.Size = new System.Drawing.Size(44, 17);
+            this.Actv.TabIndex = 5;
+            this.Actv.Text = "Activity";
+            this.Actv.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Start Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(212, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "End Time";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(759, 432);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Khaki;
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.AddActv);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.dateTimePicker1);
+            this.tabPage1.Controls.Add(this.ETimeTB);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.Actv);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.STimeTB);
+            this.tabPage1.Controls.Add(this.ActTB);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(751, 406);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Adding An Activity";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Khaki;
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.dateTimePicker3);
+            this.tabPage2.Controls.Add(this.Show_Button);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Controls.Add(this.dateTimePicker2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(751, 406);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Displaying Activities";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 26);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Submit Changes";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Location = new System.Drawing.Point(389, 45);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker3.TabIndex = 4;
+            // 
+            // Show_Button
+            // 
+            this.Show_Button.Location = new System.Drawing.Point(11, 91);
+            this.Show_Button.Name = "Show_Button";
+            this.Show_Button.Size = new System.Drawing.Size(75, 23);
+            this.Show_Button.TabIndex = 3;
+            this.Show_Button.Text = "Show";
+            this.Show_Button.UseVisualStyleBackColor = true;
+            this.Show_Button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(634, 205);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(11, 45);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 1;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Choose a Date ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(578, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = " Haf (hafizeltayeb@gmail.com)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 425);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "My Activities Planner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AddActv;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox ActTB;
+        private System.Windows.Forms.TextBox STimeTB;
+        private System.Windows.Forms.TextBox ETimeTB;
+        private System.Windows.Forms.Label Actv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button Show_Button;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MyActivitiesPlanner_version1/MyActivitiesPlanner/MyActivitiesPlanner/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MyActivitiesPlanner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+            string connectionString = @"Data Source=NEXIUS-01806;Initial Catalog=ActivitiesDB;Integrated Security=True";
+            SqlConnection sqlCon = new SqlConnection(connectionString);
+            string commandString = "INSERT INTO [ActivitiesDB].[dbo].[Activities] (Date,StartTime,EndTime,Activity) VALUES ( @Date,@STime, @ETime, @Activity)";
+              SqlCommand sqlCmd = new SqlCommand(commandString, sqlCon);   
+              sqlCmd.Parameters.AddWithValue("@Date",dateTimePicker1.Value);
+              sqlCmd.Parameters.AddWithValue("@STime", STimeTB.Text);
+              sqlCmd.Parameters.AddWithValue("@ETime", ETimeTB.Text);
+              sqlCmd.Parameters.AddWithValue("@Activity",ActTB.Text);
+            sqlCon.Open();
+            sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
+            MessageBox.Show("Activity has been added successfully");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable dt= new DataTable();
+            try
+            {
+                string connectionString = @"Data Source=NEXIUS-01806;Initial Catalog=ActivitiesDB;Integrated Security=True";
+                SqlConnection sqlCon = new SqlConnection(connectionString);
+                string commandString = "SELECT ActID 'ID', Date, StartTime 'Start Time', EndTime 'End Time', Activity, CompletionRate 'Completion Rate (%)', Completed 'Is Completed' FROM [ActivitiesDB].[dbo].[Activities] WHERE (Date >= @dateTimePicker2 AND date <= @dateTimePicker3)";
+                SqlCommand sqlCmd = new SqlCommand(commandString, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@dateTimePicker2", dateTimePicker2.Value.AddHours(-12));
+                sqlCmd.Parameters.AddWithValue("@dateTimePicker3", dateTimePicker3.Value);
+
+                sqlCon.Open();
+                SqlDataAdapter da = new SqlDataAdapter(sqlCmd);
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                sqlCon.Close();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                    string connectionString = @"Data Source=NEXIUS-01806;Initial Catalog=ActivitiesDB;Integrated Security=True";
+                    SqlConnection sqlCon = new SqlConnection(connectionString);
+                    string commandString = "UPDATE [ActivitiesDB].[dbo].[Activities] SET CompletionRate= @compRate, Completed=@completed WHERE ActID= @id";
+                    SqlCommand sqlCmd = new SqlCommand(commandString, sqlCon);
+                    SqlParameter compRate = new SqlParameter("@compRate",SqlDbType.Float);
+                    SqlParameter completed = new SqlParameter("@completed", SqlDbType.Bit);
+                    SqlParameter id = new SqlParameter("@id", SqlDbType.Int);
+                    sqlCmd.Parameters.Add(compRate);
+                    sqlCmd.Parameters.Add(completed);
+                    sqlCmd.Parameters.Add(id);
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        if (!row.IsNewRow)
+                        {
+                            //sqlCmd.Parameters.AddWithValue("@compRate", dataGridView1.CurrentRow.Cells["CompletionRate"].Value);
+                            //sqlCmd.Parameters.AddWithValue("@completed", dataGridView1.CurrentRow.Cells["Completed"].Value);
+                            //sqlCmd.Parameters.AddWithValue("@id", dataGridView1.CurrentRow.Cells["ActID"].Value);
+                            compRate.Value= row.Cells["Completion Rate (%)"].Value;
+                            completed.Value = row.Cells["Is Completed"].Value;
+                            id.Value = row.Cells["ID"].Value;
+
+                            sqlCon.Open();
+                            sqlCmd.ExecuteNonQuery();
+                            sqlCon.Close();
+                        }
+                    }
+                    
+                    MessageBox.Show("Activities has been updated successfully");
+                }
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
